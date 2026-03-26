eccd38bc64410ab906abe81c9a6199abf1c7e472
Ashok J <ashokj.ec23@bitsathy.ac.in>
2026-03-26 11:37:21 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     console.log('Connecting to MongoDB...');
     console.log('URI:', process.env.MONGODB_URI);
     
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
